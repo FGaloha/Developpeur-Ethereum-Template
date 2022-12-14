d82c6ff60b99aebe98f53dae2480275d02cc054a
Flavia Gallois <fgbis@hotmail.com>
2022-12-14 09:47:45 +0100

    hardhat tests

--- 4. Truffle & CI-CD/hardat_exo1/scripts/deploy.js
+++ 4. Truffle & CI-CD/hardat_exo1/scripts/deploy.js
@@ -9,16 +9,6 @@ async function main() {
 
   console.log(`Contract deployed at address : ${hello.address}`)
 
-  // const _money = "1";
-  // const _number = 7;
-  // await deployer.deploy(SimpleStorage, _number, { from: account[0], value: _money });
-  // let instance = await SimpleStorage.deployed();
-  // let value = await instance.get();
-  // console.log(value.toString())
-  // await instance.set(444);
-  // value = await instance.get();
-  // console.log(value.toString());
-
 }
 
 main().catch((error) => {

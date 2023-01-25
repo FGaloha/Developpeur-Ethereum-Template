f5d4640b7c886a58a2be4a83a8eefb6972a7284b
Flavia Gallois <fgbis@hotmail.com>
2023-01-25 13:34:55 +0100

    limit event logs 4

--- 6. Dapp/job-chakra-wagmi/front/components/List/ListActive.jsx
+++ 6. Dapp/job-chakra-wagmi/front/components/List/ListActive.jsx
@@ -35,7 +35,7 @@ export const ListActive = ({ address, contractAddress }) => {
     //   address: contractAddress,
     //   fromBlock: 8373513
     // }
-    const fullEventJobs = await contract.queryFilter("*", -4998, 'latest')
+    const fullEventJobs = await contract.queryFilter("*", -3000, 'latest')
     let EventsJobAddedActive = []
     let EventsJobTakenActive = []
     let EventsJobFinished = []
